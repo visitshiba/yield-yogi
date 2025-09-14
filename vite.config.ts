718185063692ad76e39f20e98c3c91e10c46cdfc
visitshiba <visitshibasahoo@gmail.com>
2025-09-14 23:32:38 +0530

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,6 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // 👇 Add this line for GitHub Pages (repo name)
+  base: "/yield-yogi/",
 }));
